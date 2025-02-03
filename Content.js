c444b66ee99ff3dcc7982543331f6f4a84cab596
Fabian <38659818+Fejbien@users.noreply.github.com>
2025-02-03 01:51:02 +0100

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,5 @@
+const playButton = document.querySelector(".fbl-play-btn");
+playButton.addEventListener("click", function () {
+    const xrayQuickView = document.querySelector(".xrayQuickView");
+    xrayQuickView.style.display = "none";
+});
